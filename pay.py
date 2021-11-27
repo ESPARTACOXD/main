59e85da12260fb77ea70b9a08fae676452e7327f
ESPARTACOXD <77385476+ESPARTACOXD@users.noreply.github.com>
2021-11-27 17:31:31 +0100

    Message

---
+++ pay.py
@@ -0,0 +1,23 @@
+import time
+
+input('-hey what do u need?\n')
+input('-oh ok bro, do u need something from me?\n')
+input('-wow no, im not a bank\n')
+input('-no, im not\n')
+input('-no, im not\n')
+input('-NO, IM NOT\n')
+input('-... ok, how much money do u need?\n')
+input('-ok bro, bye, see you later\n')
+input('-wtf bro are u kidding me?!?!\n')
+input('-u will need to work\n')
+input('-i don\'t want to give u that amount of money\n')
+input('-nop, she isn\'t my friend\n')
+input('-BUT NOT MY FRIEND, I DONT KNOW HER\n')
+input('-AAAAAAAAAAAAAAAAAAAA\n')
+input('-AAA. OK, let\'s calm down, i will pay\n')
+input('-yeah, but leave me alone, ok?\n')
+input('-well, here it comes\n')
+time.sleep(1)
+print('*the transfer was successful*')
+input('-well, bye and fuck you\n')
+
