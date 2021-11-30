12dbd1fde9035d01535c358a76e10bffd195aca4
ESPARTACOXD <77385476+ESPARTACOXD@users.noreply.github.com>
2021-11-30 17:59:44 +0100

    main

---
+++ roll/script.js
@@ -0,0 +1,20 @@
+document.getElementById("button").addEventListener("click", function() {
+    // Change background to dancing rick
+    document.body.style.backgroundImage = "url('rick.gif')"
+  
+    // Hide the heading and the button
+    document.querySelector(".everything").style.display = "none"
+  
+    // Play song
+    var song = document.getElementById("song")
+    song.play()
+  
+    bttntimeout = setTimeout(function () {
+      document.querySelector(".real").style.display = "flex"
+      document.querySelector(".real").style.color = "white"
+      document.querySelector(".real").style.backgroundColor = "rgb(0,0,0)"
+      document.querySelector(".everything").style.display = "flex"
+      document.getElementById("title").style.display = "none"
+      document.getElementById("button").style.display = "none"
+    }, 2000);
+  })
